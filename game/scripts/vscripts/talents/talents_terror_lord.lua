860b5e1b38963dbbd8219356ec87d231fab5583a
fdsatrew <fdsatrewq3@gmail.com>
2020-04-23 21:37:14 +0300

    - Added talent 46 - Icon for talent 46 - Tooltips

--- game/scripts/vscripts/talents/talents_terror_lord.lua
+++ game/scripts/vscripts/talents/talents_terror_lord.lua
@@ -1792,7 +1792,7 @@ modifier_npc_dota_hero_abyssal_underlord_talent_45_debuff = modifier_npc_dota_he
 })
 
 function modifier_npc_dota_hero_abyssal_underlord_talent_45_debuff:OnCreated()
-    if(not IsServer()) then
+    if (not IsServer()) then
         return
     end
     self.elementarmor_reduction = 0.1
@@ -1827,6 +1827,41 @@ function modifier_npc_dota_hero_abyssal_underlord_talent_45_debuff:GetInfernoPro
 end
 
 LinkedModifiers["modifier_npc_dota_hero_abyssal_underlord_talent_45_debuff"] = LUA_MODIFIER_MOTION_NONE
+
+-- modifier_npc_dota_hero_abyssal_underlord_talent_46 (Nova Terror)
+modifier_npc_dota_hero_abyssal_underlord_talent_46 = modifier_npc_dota_hero_abyssal_underlord_talent_46 or class({
+    IsDebuff = function(self)
+        return false
+    end,
+    IsHidden = function(self)
+        return true
+    end,
+    IsPurgable = function(self)
+        return false
+    end,
+    RemoveOnDeath = function(self)
+        return false
+    end,
+    AllowIllusionDuplicate = function(self)
+        return false
+    end,
+    GetAttributes = function(self)
+        return MODIFIER_ATTRIBUTE_PERMANENT
+    end
+})
+
+function modifier_npc_dota_hero_abyssal_underlord_talent_46:OnCreated()
+    if (not IsServer()) then
+        return
+    end
+    self.caster = self:GetParent()
+end
+
+function modifier_npc_dota_hero_abyssal_underlord_talent_46:GetFireDamageBonus()
+    return Units:GetInfernoDamage(self.caster) * 0.2
+end
+
+LinkedModifiers["modifier_npc_dota_hero_abyssal_underlord_talent_46"] = LUA_MODIFIER_MOTION_NONE
 
 -- Internal stuff
 for LinkedModifier, MotionController in pairs(LinkedModifiers) do
