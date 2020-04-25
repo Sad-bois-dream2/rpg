cfec38a83c81b83a220c420dce24c8276186da6b
fdsatrew <fdsatrewq3@gmail.com>
2020-04-25 16:29:02 +0300

    - Particle for invoker 1st skill v0.0001

--- game/scripts/vscripts/heroes/hero_chosen_invoker.lua
+++ game/scripts/vscripts/heroes/hero_chosen_invoker.lua
@@ -1,11 +1,37 @@
 local LinkedModifiers = {}
 
 -- chosen_invoker_purification_brilliance modifiers
+modifier_chosen_invoker_purification_brilliance = modifier_chosen_invoker_purification_brilliance or class({
+    IsDebuff = function(self)
+        return false
+    end,
+    IsHidden = function(self)
+        return false
+    end,
+    IsPurgable = function(self)
+        return false
+    end,
+    RemoveOnDeath = function(self)
+        return true
+    end,
+    AllowIllusionDuplicate = function(self)
+        return false
+    end,
+    GetTexture = function(self)
+        return chosen_invoker_purification_brilliance:GetAbilityTextureName()
+    end,
+    GetEffectName = function(self)
+        return "particles/units/blazing_berserker/furious_stance/furious_stance.vpcf"
+    end
+})
 
 -- chosen_invoker_purification_brilliance
 chosen_invoker_purification_brilliance = class({
     GetAbilityTextureName = function(self)
         return "chosen_invoker_purification_brilliance"
+    end,
+    IsRequireCastbar = function(self)
+        return true
     end
 })
 
