da0f8afa23dfcd287d5a9fe2e92a4aaa515d750c
fdsatrew <fdsatrewq3@gmail.com>
2020-04-27 11:09:37 +0300

    - Added first version of creep scaling

--- game/scripts/vscripts/systems/enemies.lua
+++ game/scripts/vscripts/systems/enemies.lua
@@ -31,14 +31,14 @@ function Enemies:InitPanaromaEvents()
 end
 
 function Enemies:IsElite(unit)
-    if(not unit or unit:IsNull()) then
+    if (not unit or unit:IsNull()) then
         return false
     end
     return false
 end
 
 function Enemies:IsBoss(unit)
-    if(not unit or unit:IsNull()) then
+    if (not unit or unit:IsNull()) then
         return false
     end
     if (string.find(unit:GetUnitName(), "boss")) then
@@ -47,7 +47,111 @@ function Enemies:IsBoss(unit)
     return false
 end
 
+modifier_creep_scaling = modifier_creep_scaling or class({
+    IsDebuff = function(self)
+        return false
+    end,
+    IsHidden = function(self)
+        return true
+    end,
+    IsPurgable = function(self)
+        return false
+    end,
+    RemoveOnDeath = function(self)
+        return false
+    end,
+    AllowIllusionDuplicate = function(self)
+        return false
+    end,
+    GetAttributes = function(self)
+        return MODIFIER_ATTRIBUTE_PERMANENT
+    end
+})
+
+function modifier_creep_scaling:GetValue(minValue, maxValue, scaling)
+    return minValue + ((maxValue - minValue) * scaling)
+end
+
+function modifier_creep_scaling:OnCreated()
+    if (not IsServer()) then
+        return
+    end
+    self.creep = self:GetParent()
+    self.name = self.creep:GetUnitName()
+    self.damage = Enemies.data[self.name]["AttackDamageMax"]
+    self.armor = 2
+    self.elementalArmor = 0.11
+    if (Enemies:IsElite(self.creep)) then
+        self.armor = 5
+        self.elementalArmor = 0.23
+    end
+    if (Enemies:IsBoss(self.creep)) then
+        self.armor = 15
+        self.elementalArmor = 0.47
+    end
+    self.difficulty = 1
+    self.damage = self.damage * math.pow(self.difficulty, 3)
+    self.armor = math.min(self.armor + ((50 - self.armor) * self.difficulty), 150)
+    self.elementalArmor = math.min(self.elementalArmor + ((0.75 - self.elementalArmor) * self.difficulty), 0.9)
+end
+
+function modifier_creep_scaling:GetAttackDamageBonus()
+    return self.damage
+end
+
+function modifier_creep_scaling:GetArmorBonus()
+    return self.armor
+end
+
+function modifier_creep_scaling:GetFireProtectionBonus()
+    return self.elementalArmor
+end
+
+function modifier_creep_scaling:GetFrostProtectionBonus()
+    return self.elementalArmor
+end
+
+function modifier_creep_scaling:GetEarthProtectionBonus()
+    return self.elementalArmor
+end
+
+function modifier_creep_scaling:GetVoidProtectionBonus()
+    return self.elementalArmor
+end
+
+function modifier_creep_scaling:GetHolyProtectionBonus()
+    return self.elementalArmor
+end
+
+function modifier_creep_scaling:GetNatureProtectionBonus()
+    return self.elementalArmor
+end
+
+function modifier_creep_scaling:GetInfernoProtectionBonus()
+    return self.elementalArmor
+end
+
+function modifier_creep_scaling:GetBaseAttackTime()
+    return 2
+end
+
+LinkLuaModifier("modifier_creep_scaling", "systems/enemies", LUA_MODIFIER_MOTION_NONE)
+
+ListenToGameEvent("npc_spawned", function(keys)
+    if (not IsServer()) then
+        return
+    end
+    local unit = EntIndexToHScript(keys.entindex)
+    local isUnitThinker = (unit:GetUnitName() == "npc_dota_thinker")
+    if (not unit:HasModifier("modifier_creep_scaling") and not Summons:IsSummmon(unit) and not isUnitThinker and unit:GetTeam() == DOTA_TEAM_NEUTRALS) then
+        unit:AddNewModifier(unit, nil, "modifier_creep_scaling", { Duration = -1 })
+    end
+end, nil)
+
+Enemies.initialized = false
+
 if not Enemies.initialized then
     Enemies:Init()
     Enemies.initialized = true
+    Enemies.data = LoadKeyValues("scripts/npc/npc_units_custom.txt")
 end
