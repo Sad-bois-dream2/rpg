a9e96a92f99d08ec5cfaefd695565a77db1823fb
fdsatrew <fdsatrewq3@gmail.com>
2020-04-29 19:14:08 +0300

    Almost working invoker first skill Reworked particle for invoker first skill

--- game/scripts/vscripts/heroes/hero_chosen_invoker.lua
+++ game/scripts/vscripts/heroes/hero_chosen_invoker.lua
@@ -6,7 +6,7 @@ modifier_chosen_invoker_purification_brilliance = modifier_chosen_invoker_purifi
         return false
     end,
     IsHidden = function(self)
-        return false
+        return true
     end,
     IsPurgable = function(self)
         return false
@@ -16,14 +16,43 @@ modifier_chosen_invoker_purification_brilliance = modifier_chosen_invoker_purifi
     end,
     AllowIllusionDuplicate = function(self)
         return false
-    end,
-    GetTexture = function(self)
-        return chosen_invoker_purification_brilliance:GetAbilityTextureName()
-    end,
-    GetEffectName = function(self)
-        return "particles/units/blazing_berserker/furious_stance/furious_stance.vpcf"
     end
 })
+
+function modifier_chosen_invoker_purification_brilliance:OnCreated()
+    if (not IsServer()) then
+        return
+    end
+    self.ability = self:GetAbility()
+    self.caster = self:GetParent()
+    self.target = self.ability:GetCursorTarget()
+    local tick = self.ability:GetSpecialValueFor("tick")
+    self:StartIntervalThink(tick)
+    self:OnIntervalThink()
+end
+
+function modifier_chosen_invoker_purification_brilliance:OnIntervalThink()
+    if (not IsServer()) then
+        return
+    end
+    self.caster:StartGesture(ACT_DOTA_CAST_ALACRITY)
+    local pidx = ParticleManager:CreateParticle("particles/units/chosen_invoker/purification_brilliance/purification_brilliance_rope.vpcf", PATTACH_POINT, self.caster)
+    ParticleManager:SetParticleControlEnt(pidx, 0, self.caster, PATTACH_POINT_FOLLOW, "attach_attack2", self.caster:GetAbsOrigin(), true)
+    ParticleManager:SetParticleControlEnt(pidx, 1, self.target, PATTACH_POINT_FOLLOW, "attach_hitloc", self.target:GetAbsOrigin(), true)
+    Timers:CreateTimer(1.0, function()
+        ParticleManager:DestroyParticle(pidx, false)
+        ParticleManager:ReleaseParticleIndex(pidx)
+    end)
+end
+
+function modifier_chosen_invoker_purification_brilliance:OnDestroy()
+    if (not IsServer()) then
+        return
+    end
+    self.caster:RemoveGesture(ACT_DOTA_CAST_ALACRITY)
+end
+
+LinkedModifiers["modifier_chosen_invoker_purification_brilliance"] = LUA_MODIFIER_MOTION_NONE
 
 -- chosen_invoker_purification_brilliance
 chosen_invoker_purification_brilliance = class({
@@ -35,7 +64,22 @@ chosen_invoker_purification_brilliance = class({
     end
 })
 
+function chosen_invoker_purification_brilliance:OnChannelFinish()
+    if (not IsServer()) then
+        return
+    end
+    self.modifier:Destroy()
+end
+
+function chosen_invoker_purification_brilliance:OnSpellStart()
+    if (not IsServer()) then
+        return
+    end
+    local caster = self:GetCaster()
+    self.modifier = caster:AddNewModifier(caster, self, "modifier_chosen_invoker_purification_brilliance", { duration = self:GetChannelTime() })
+end
+
 -- Internal stuff
 for LinkedModifier, MotionController in pairs(LinkedModifiers) do
-    LinkLuaModifier(LinkedModifier, "heroes/hero_blazing_berserker", MotionController)
+    LinkLuaModifier(LinkedModifier, "heroes/hero_chosen_invoker", MotionController)
 end
