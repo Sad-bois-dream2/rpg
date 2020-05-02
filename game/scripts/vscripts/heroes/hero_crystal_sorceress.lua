12a7fda5117695938955e5589026c5fbdeb72792
fdsatrew <fdsatrewq3@gmail.com>
2020-05-02 19:50:31 +0300

    Almost working first skill Particle for first skill

--- game/scripts/vscripts/heroes/hero_crystal_sorceress.lua
+++ game/scripts/vscripts/heroes/hero_crystal_sorceress.lua
@@ -1,7 +1,5 @@
 local LinkedModifiers = {}
 
--- crystal_sorceress_frost_comet modifiers
-
 -- crystal_sorceress_frost_comet
 crystal_sorceress_frost_comet = class({
     GetAbilityTextureName = function(self)
@@ -11,6 +9,64 @@ crystal_sorceress_frost_comet = class({
         return true
     end
 })
+
+function crystal_sorceress_frost_comet:OnAbilityPhaseStart()
+    if (not IsServer()) then
+        return true
+    end
+    self.caster = self:GetCaster()
+    self.target = self:GetCursorTarget()
+    local casterPosition = self.caster:GetAbsOrigin()
+    local targetPosition = self.target:GetAbsOrigin()
+    local direction = (targetPosition - casterPosition):Normalized()
+    self.orbPosition = casterPosition + (direction * 100) + Vector(0, 0, 200)
+    local lifeDuration = 2
+    self.pidx = ParticleManager:CreateParticle("particles/units/crystal_sorceress/crystal_sorceress_frost_comet_cast.vpcf", PATTACH_POINT, self.caster)
+    ParticleManager:SetParticleControl(self.pidx, 0, self.orbPosition)
+    ParticleManager:SetParticleControl(self.pidx, 1, Vector(0, lifeDuration, 0))
+    EmitSoundOnLocationWithCaster(self.orbPosition, "Hero_Ancient_Apparition.ColdFeetCast", self.caster)
+    Timers:CreateTimer(lifeDuration, function()
+        if (self.pidx) then
+            ParticleManager:DestroyParticle(self.pidx, false)
+            ParticleManager:ReleaseParticleIndex(self.pidx)
+        end
+        StopSoundOn("Hero_Ancient_Apparition.ColdFeetCast", self.caster)
+    end, self)
+    return true
+end
+
+function crystal_sorceress_frost_comet:OnAbilityPhaseInterrupted()
+    if (not IsServer()) then
+        return
+    end
+    ParticleManager:DestroyParticle(self.pidx, true)
+    ParticleManager:ReleaseParticleIndex(self.pidx)
+end
+
+function crystal_sorceress_frost_comet:OnProjectileHit()
+    if (not IsServer()) then
+        return
+    end
+
+    EmitSoundOn("Hero_Ancient_Apparition.ChillingTouch.Target", self.target)
+end
+
+function crystal_sorceress_frost_comet:OnSpellStart()
+    if (not IsServer()) then
+        return
+    end
+    local projectile = {
+        Target = self.target,
+        Ability = self,
+        vSourceLoc = self.orbPosition,
+        EffectName = "particles/units/crystal_sorceress/crystal_sorceress_frost_comet.vpcf",
+        bDodgable = false,
+        bProvidesVision = false,
+        iMoveSpeed = 800
+    }
+    ProjectileManager:CreateTrackingProjectile(projectile)
+    EmitSoundOn("Hero_Ancient_Apparition.ChillingTouch.Cast", self.caster)
+end
 
 -- Internal stuff
 for LinkedModifier, MotionController in pairs(LinkedModifiers) do
