ce678d8fd70410a4b3864e9b7b22151f9cf773de
fdsatrew <fdsatrewq3@gmail.com>
2020-05-06 16:48:48 +0300

    Added template

---
+++ game/scripts/vscripts/heroes/hero_luminous_samurai.lua
@@ -0,0 +1,13 @@
+local LinkedModifiers = {}
+
+-- luminous_samurai_bankai
+luminous_samurai_bankai = class({
+    GetAbilityTextureName = function(self)
+        return "luminous_samurai_bankai"
+    end
+})
+
+-- Internal stuff
+for LinkedModifier, MotionController in pairs(LinkedModifiers) do
+    LinkLuaModifier(LinkedModifier, "heroes/hero_luminous_samurai", MotionController)
+end
