ec7d36858140ec0aba8bf0841e9f39fdd52955b8
fdsatrew <fdsatrewq3@gmail.com>
2020-05-06 17:06:47 +0300

    More work on first ability

--- game/scripts/vscripts/heroes/hero_luminous_samurai.lua
+++ game/scripts/vscripts/heroes/hero_luminous_samurai.lua
@@ -1,4 +1,47 @@
 local LinkedModifiers = {}
+-- luminous_samurai_bankai modifiers
+modifier_luminous_samurai_bankai_buff = class({
+    IsDebuff = function(self)
+        return false
+    end,
+    IsHidden = function(self)
+        return false
+    end,
+    IsPurgable = function(self)
+        return false
+    end,
+    RemoveOnDeath = function(self)
+        return true
+    end,
+    AllowIllusionDuplicate = function(self)
+        return false
+    end,
+    GetTexture = function(self)
+        return luminous_samurai_bankai:GetAbilityTextureName()
+    end
+})
+
+LinkedModifiers["modifier_luminous_samurai_bankai_buff"] = LUA_MODIFIER_MOTION_NONE
+
+modifier_luminous_samurai_bankai = class({
+    IsDebuff = function(self)
+        return false
+    end,
+    IsHidden = function(self)
+        return true
+    end,
+    IsPurgable = function(self)
+        return false
+    end,
+    RemoveOnDeath = function(self)
+        return true
+    end,
+    AllowIllusionDuplicate = function(self)
+        return false
+    end
+})
+
+LinkedModifiers["modifier_luminous_samurai_bankai"] = LUA_MODIFIER_MOTION_NONE
 
 -- luminous_samurai_bankai
 luminous_samurai_bankai = class({
@@ -6,6 +49,20 @@ luminous_samurai_bankai = class({
         return "luminous_samurai_bankai"
     end
 })
+
+function luminous_samurai_bankai:OnSpellStart()
+    if(not IsServer()) then
+        return
+    end
+    local caster = self:GetCaster()
+    local modifierTable = {}
+    modifierTable.ability = self
+    modifierTable.target = caster
+    modifierTable.caster = caster
+    modifierTable.modifier_name = "modifier_luminous_samurai_bankai"
+    modifierTable.duration = self:GetSpecialValueFor("duration")
+    GameMode:ApplyBuff(modifierTable)
+end
 
 -- Internal stuff
 for LinkedModifier, MotionController in pairs(LinkedModifiers) do
