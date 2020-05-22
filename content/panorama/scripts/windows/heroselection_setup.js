d6c9566f77e44d98474bbc71b40ba895b8265195
fdsatrew <fdsatrewq3@gmail.com>
2020-05-22 20:19:25 +0300

    Finished top icons for players

--- content/panorama/scripts/windows/heroselection_setup.js
+++ content/panorama/scripts/windows/heroselection_setup.js
@@ -2,7 +2,6 @@ function UpdateTimer()
 {
 	var gameTime = Game.GetGameTime();
 	var transitionTime = Game.GetStateTransitionTime();
-	$.Msg(transitionTime);
 	if ( transitionTime >= 0 )
 	{
 		$("#TimerLabel").SetDialogVariableInt( "countdown_timer_seconds", Math.max( 0, Math.floor( transitionTime - gameTime ) ) );
@@ -10,6 +9,45 @@ function UpdateTimer()
 	$.Schedule( 0.1, UpdateTimer );
 }
 
+function GetHEXPlayerColor(playerId) {
+	var playerColor = Players.GetPlayerColor(playerId).toString(16);
+	return playerColor == null ? '#000000' : ('#' + playerColor.substring(6, 8) + playerColor.substring(4, 6) + playerColor.substring(2, 4) + playerColor.substring(0, 2));
+}
+
+function HideDefaultUI()
+{
+	var defaultGameSetup = $.GetContextPanel().GetParent().FindChildTraverse('TeamSelectContainer');
+	if(defaultGameSetup) {
+	    defaultGameSetup.style.visibility = "collapse";
+	    defaultGameSetup.hittest = false;
+	    defaultGameSetup.hittestchildren = false;
+	} else {
+	    $.Schedule( 0.1, HideDefaultUI );
+	}
+}
+
+function UpdateHeroColors() {
+    var loadedPlayers = 0;
+    for(var i =0; i < 5; i++) {
+        var playerColor = GetHEXPlayerColor(i);
+        if(playerColor != "#ffffffff") {
+            loadedPlayers++;
+        }
+        $("#HeroColor"+i).style.backgroundColor = playerColor;
+        var playerName = Players.GetPlayerName(i);
+        if(playerName.length > 0) {
+            $("#HeroName" + i).text = playerName;
+        } else {
+            $("#HeroName" + i).GetParent().style.visibility = "collapse";
+        }
+    }
+    if(loadedPlayers < 1) {
+	    $.Schedule( 0.1, UpdateHeroColors );
+    }
+}
+
 (function() {
 	UpdateTimer();
+	UpdateHeroColors();
+	HideDefaultUI();
 })();
