8703c11365b83e6331ebc94ef3ee5e46bfaefda6
fdsatrew <fdsatrewq3@gmail.com>
2020-05-23 16:12:49 +0300

    Finished UI part

--- content/panorama/scripts/windows/heroselection_setup.js
+++ content/panorama/scripts/windows/heroselection_setup.js
@@ -1,7 +1,74 @@
+var heroesData = {
+    "npc_dota_hero_drow_ranger" : {
+        "tank" : 0,
+        "dps" : 5,
+        "support" : 0
+    },
+    "npc_dota_hero_abyssal_underlord" : {
+        "tank" : 5,
+        "dps" : 3,
+        "support" : 0
+    },
+    "npc_dota_hero_invoker" : {
+        "tank" : 0,
+        "dps" : 5,
+        "support" : 0
+    },
+    "npc_dota_hero_dark_willow" : {
+        "tank" : 0,
+        "dps" : 5,
+        "support" : 0
+    },
+    "npc_dota_hero_silencer" : {
+        "tank" : 0,
+        "dps" : 2,
+        "support" : 5
+    },
+    "npc_dota_hero_phantom_assassin" : {
+        "tank" : 0,
+        "dps" : 5,
+        "support" : 0
+    },
+    "npc_dota_hero_mars" : {
+        "tank" : 5,
+        "dps" : 2,
+        "support" : 0
+    },
+    "npc_dota_hero_doom_bringer" : {
+        "tank" : 0,
+        "dps" : 5,
+        "support" : 0
+    },
+    "npc_dota_hero_axe" : {
+        "tank" : 0,
+        "dps" : 5,
+        "support" : 0
+    },
+    "npc_dota_hero_crystal_maiden" : {
+        "tank" : 0,
+        "dps" : 5,
+        "support" : 0
+    },
+    "npc_dota_hero_enchantress" : {
+        "tank" : 0,
+        "dps" : 0,
+        "support" : 5
+    },
+    "npc_dota_hero_juggernaut" : {
+        "tank" : 0,
+        "dps" : 5,
+        "support" : 0
+    },
+};
+
+var MAX_STATS = 5;
+var tankStats, dpsStats, supportStats;
+
 function OnHeroSelected(event) {
     $("#HeroIcon" + event.player_id).heroname = event.hero;
     if(event.player_id == Players.GetLocalPlayer()) {
         $("#SelectedHeroName").text = $.Localize("#" + event.hero);
+        UpdateHeroStats(event.hero);
     }
 }
 
@@ -9,6 +76,24 @@ function OnHeroPicked(event) {
     var heroIcon = $("#HeroIcon" + event.player_id);
     heroIcon.heroname = event.hero;
     heroIcon.SetHasClass("notpicked", false);
+}
+
+function UpdateHeroStats(hero) {
+    var tank = 0;
+    var dps = 0;
+    var support = 0;
+    if(heroesData[hero]) {
+        tank = heroesData[hero]["tank"];
+        dps = heroesData[hero]["dps"];
+        support = heroesData[hero]["support"];
+    }
+    for(var i = 1; i <= MAX_STATS; i++) {
+        var index = i - 1;
+        var statValue = index + 1;
+        tankStats[index].SetHasClass("selected", (statValue <= tank));
+        dpsStats[index].SetHasClass("selected", (statValue <= dps));
+        supportStats[index].SetHasClass("selected", (statValue <= support));
+    }
 }
 
 function UpdateTimer()
@@ -68,5 +153,16 @@ function FixGameSetupWindow() {
 	UpdateHeroColors();
 	HideDefaultUI();
     FixGameSetupWindow();
+    var tankContainer = $("#TankStat");
+    var dpsContainer = $("#DpsStat");
+    var supportContainer = $("#SupportStat");
+    tankStats = [];
+    supportStats = [];
+    dpsStats = [];
+    for(var i = 0; i < MAX_STATS; i++) {
+        tankStats.push(tankContainer.GetChild(i));
+        dpsStats.push(dpsContainer.GetChild(i));
+        supportStats.push(supportContainer.GetChild(i));
+    }
 	GameEvents.Subscribe("rpg_hero_selection_hero_selected_from_server", OnHeroSelected);
 })();
