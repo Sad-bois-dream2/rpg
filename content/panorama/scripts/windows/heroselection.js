a63a2a9c78c9bdfd652076329089815a7e6a2c7c
fdsatrew <fdsatrewq3@gmail.com>
2020-05-23 14:42:10 +0300

    Improved layout a bit

--- content/panorama/scripts/windows/heroselection.js
+++ content/panorama/scripts/windows/heroselection.js
@@ -1,11 +1,28 @@
-var heroesContainer;
+var heroes = [];
+var heroScreenLoaded = false;
 
 function OnHeroSelected(hero) {
-    $.Msg(hero);
+	$.DispatchEvent('DOTAGlobalSceneSetCameraEntity', 'HeroSelectionScreen', hero, 0.5);
+}
+
+function OnHeroSelectionScreenLoaded() {
+    $("#Spinner").style.visibility = "collapse";
+    $("#AvailableHeroesContainer").style.visibility = "visible";
+    $("#HeroControls").style.visibility = "visible";
+    heroScreenLoaded = true;
+}
+
+function WaitHeroScreenLoading() {
+    if(heroScreenLoaded) {
+	    $.DispatchEvent('DOTAGlobalSceneSetCameraEntity', 'HeroSelectionScreen', heroes[0], 0);
+    } else {
+        $.Schedule( 0.1, WaitHeroScreenLoading );
+    }
 }
 
 function OnHeroesDataRecieved(event) {
-    var heroes = JSON.parse(event.heroes);
+    var heroesContainer = $("#AvailableHeroes");
+    heroes = JSON.parse(event.heroes);
     for(var i = 0; i < heroes.length; i++) {
 		var heroPanel = $.CreatePanel( "RadioButton", heroesContainer, heroes[i]);
         heroPanel.BLoadLayout("file://{resources}/layout/custom_game/windows/heroselection/heroselection_button.xml", false, false);
@@ -15,7 +32,7 @@ function OnHeroesDataRecieved(event) {
 }
 
 (function() {
-    heroesContainer = $("#AvailableHeroes");
 	GameEvents.SendCustomGameEventToServer("rpg_hero_selection_get_heroes",{});
 	GameEvents.Subscribe("rpg_hero_selection_get_heroes_from_server", OnHeroesDataRecieved);
+	WaitHeroScreenLoading();
 })();
