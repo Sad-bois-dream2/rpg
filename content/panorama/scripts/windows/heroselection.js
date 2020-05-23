c29b74927a4456c2eb5a43c51a295ed6b24ab182
fdsatrew <fdsatrewq3@gmail.com>
2020-05-23 14:05:39 +0300

    Added tooltips

--- content/panorama/scripts/windows/heroselection.js
+++ content/panorama/scripts/windows/heroselection.js
@@ -1,11 +1,16 @@
 var heroesContainer;
 
+function OnHeroSelected(hero) {
+    $.Msg(hero);
+}
+
 function OnHeroesDataRecieved(event) {
     var heroes = JSON.parse(event.heroes);
     for(var i = 0; i < heroes.length; i++) {
 		var heroPanel = $.CreatePanel( "RadioButton", heroesContainer, heroes[i]);
         heroPanel.BLoadLayout("file://{resources}/layout/custom_game/windows/heroselection/heroselection_button.xml", false, false);
         heroPanel.FindChildTraverse('HeroIcon').heroname = heroes[i];
+        heroPanel.Data().OnHeroSelected = OnHeroSelected;
     }
 }
 
