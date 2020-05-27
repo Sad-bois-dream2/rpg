87b6dea9c4caf456c3c47c7d1fdf30dc59a9e570
fdsatrew <fdsatrewq3@gmail.com>
2020-05-27 15:05:11 +0300

    SaveLoad UI v0.04

--- content/panorama/scripts/windows/saveload.js
+++ content/panorama/scripts/windows/saveload.js
@@ -1,4 +1,14 @@
+var heroInfoPanel;
+
+function ShowHeroInfo() {
+    $.Msg($.GetContextPanel());
+    heroInfoPanel.style.visibility = "visible";
+}
+
+function HideHeroInfo() {
+    heroInfoPanel.style.visibility = "collapse";
+}
 
 (function() {
-
+    heroInfoPanel = $("#SaveSlotHeroPreview");
 })();
