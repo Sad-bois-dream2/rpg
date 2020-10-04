2d79325494375930bf2f2484d4fc0a167524c451
fdsatrew <fdsatrewq3@gmail.com>
2020-10-04 11:41:25 +0300

    Small adjustment

--- game/scripts/vscripts/generic/modifier_charges.lua
+++ game/scripts/vscripts/generic/modifier_charges.lua
@@ -1,65 +1,81 @@
-modifier_charges = class({})
+modifier_charges = class({
+    IsDebuff = function(self)
+        return false
+    end,
+    IsHidden = function(self)
+        return false
+    end,
+    IsPurgable = function(self)
+        return false
+    end,
+    RemoveOnDeath = function(self)
+        return false
+    end,
+    AllowIllusionDuplicate = function(self)
+        return false
+    end,
+    DeclareFunctions = function(self)
+        return { MODIFIER_EVENT_ON_ABILITY_FULLY_CAST }
+    end,
+    GetAttributes = function(self)
+        return MODIFIER_ATTRIBUTE_PERMANENT
+    end,
+    DestroyOnExpire = function(self)
+        return false
+    end,
 
-if IsServer() then
-    function modifier_charges:Update()
-        if self:GetDuration() == -1 then
-            self:SetDuration(self.kv.replenish_time, true)
-            self:StartIntervalThink(self.kv.replenish_time)
-        end
+})
+
+function modifier_charges:Update()
+    if (not IsServer()) then
+        return
     end
-
-    function modifier_charges:OnCreated(kv)
-        self:SetStackCount(kv.start_count or kv.max_count)
-        self.kv = kv
-        if kv.start_count and kv.start_count ~= kv.max_count then
-            self:Update()
-        end
-        self.ability = self:GetAbility()
-        self.caster = self.ability:GetCaster()
-    end
-
-    function modifier_charges:DeclareFunctions()
-        local funcs = {
-            MODIFIER_EVENT_ON_ABILITY_FULLY_CAST
-        }
-        return funcs
-    end
-
-    function modifier_charges:OnAbilityFullyCast(params)
-        if (params.unit == self.caster and params.ability == self.ability) then
-            if (self:GetStackCount() > 0) then
-                self.ability:EndCooldown()
-            else
-                self.ability:StartCooldown(self:GetRemainingTime() * Units:GetCooldownReduction(self.caster))
-            end
-            self:DecrementStackCount()
-            self:Update()
-        end
-    end
-
-    function modifier_charges:OnIntervalThink()
-        local stacks = self:GetStackCount()
-
-        if stacks < self.kv.max_count then
-            self:SetDuration(self.kv.replenish_time, true)
-            self:IncrementStackCount()
-
-            if stacks == self.kv.max_count - 1 then
-                self:SetDuration(-1, true)
-                self:StartIntervalThink(-1)
-            end
-        end
+    if self:GetDuration() == -1 then
+        self:SetDuration(self.kv.replenish_time, true)
+        self:StartIntervalThink(self.kv.replenish_time)
     end
 end
 
-function modifier_charges:DestroyOnExpire()
-    return false
+function modifier_charges:OnCreated(kv)
+    if (not IsServer()) then
+        return
+    end
+    self:SetStackCount(kv.start_count or kv.max_count)
+    self.kv = kv
+    if kv.start_count and kv.start_count ~= kv.max_count then
+        self:Update()
+    end
+    self.ability = self:GetAbility()
+    self.caster = self.ability:GetCaster()
 end
 
-function modifier_charges:IsPurgable()
-    return false
+function modifier_charges:OnAbilityFullyCast(params)
+    if (not IsServer()) then
+        return
+    end
+    if (params.unit == self.caster and params.ability == self.ability) then
+        if (self:GetStackCount() > 0) then
+            self.ability:EndCooldown()
+        else
+            self.ability:StartCooldown(self:GetRemainingTime() * Units:GetCooldownReduction(self.caster))
+        end
+        self:DecrementStackCount()
+        self:Update()
+    end
 end
 
-function modifier_charges:RemoveOnDeath()
-    return false
+function modifier_charges:OnIntervalThink()
+    if (not IsServer()) then
+        return
+    end
+    local stacks = self:GetStackCount()
+    if stacks < self.kv.max_count then
+        self:SetDuration(self.kv.replenish_time, true)
+        self:IncrementStackCount()
+
+        if stacks == self.kv.max_count - 1 then
+            self:SetDuration(-1, true)
+            self:StartIntervalThink(-1)
+        end
+    end
 end
