4373cfc6b5a8a1224907b4f103a99ef6b07de64a
fdsatrew <fdsatrewq3@gmail.com>
2020-10-20 14:55:52 +0300

    Reworked cm first ability

--- game/scripts/vscripts/heroes/hero_crystal_sorceress.lua
+++ game/scripts/vscripts/heroes/hero_crystal_sorceress.lua
@@ -1,11 +1,45 @@
 local LinkedModifiers = {}
 
 -- crystal_sorceress_frost_comet
-crystal_sorceress_frost_comet = class({
-    GetAbilityTextureName = function(self)
-        return "crystal_sorceress_frost_comet"
+modifier_crystal_sorceress_frost_comet_debuff = class({
+    IsDebuff = function(self)
+        return true
     end,
-    IsRequireCastbar = function(self)
+    IsHidden = function(self)
+        return false
+    end,
+    IsPurgable = function(self)
+        return false
+    end,
+    RemoveOnDeath = function(self)
+        return true
+    end,
+    AllowIllusionDuplicate = function(self)
+        return false
+    end,
+    DeclareFunctions = function()
+        return {
+            MODIFIER_PROPERTY_TOOLTIP
+        }
+    end
+})
+
+function modifier_crystal_sorceress_frost_comet_debuff:OnCreated()
+    self.ability = self:GetAbility()
+end
+
+function modifier_crystal_sorceress_frost_comet_debuff:GetFrostProtectionBonus()
+    return self.ability.frostResistanceReduction
+end
+
+function modifier_crystal_sorceress_frost_comet_debuff:OnTooltip()
+    return self:GetAbility():GetSpecialValueFor("frost_resistance_reduction")
+end
+
+LinkedModifiers["modifier_crystal_sorceress_frost_comet_debuff"] = LUA_MODIFIER_MOTION_NONE
+
+crystal_sorceress_frost_comet = class({
+    IsRequireCastbar = function()
         return true
     end
 })
@@ -20,18 +54,11 @@ function crystal_sorceress_frost_comet:OnAbilityPhaseStart()
     local targetPosition = self.target:GetAbsOrigin()
     local direction = (targetPosition - casterPosition):Normalized()
     self.orbPosition = casterPosition + (direction * 100) + Vector(0, 0, 200)
-    local lifeDuration = 2 * Units:GetSpellHaste(self.caster)
+    local lifeDuration = ((100 - math.abs(modifier_castbar.GetModifierPercentageCasttime({ hero = self.caster }))) / 100) * 2
     self.pidx = ParticleManager:CreateParticle("particles/units/crystal_sorceress/frost_comet/crystal_sorceress_frost_comet_cast.vpcf", PATTACH_POINT, self.caster)
     ParticleManager:SetParticleControl(self.pidx, 0, self.orbPosition)
     ParticleManager:SetParticleControl(self.pidx, 1, Vector(0, lifeDuration, 0))
-    EmitSoundOnLocationWithCaster(self.orbPosition, "Hero_Ancient_Apparition.ColdFeetCast", self.caster)
-    Timers:CreateTimer(lifeDuration, function()
-        if (self.pidx) then
-            ParticleManager:DestroyParticle(self.pidx, false)
-            ParticleManager:ReleaseParticleIndex(self.pidx)
-        end
-        StopSoundOn("Hero_Ancient_Apparition.ColdFeetCast", self.caster)
-    end, self)
+    EmitSoundOn("Hero_Ancient_Apparition.ColdFeetCast", self.caster)
     return true
 end
 
@@ -39,34 +66,36 @@ function crystal_sorceress_frost_comet:OnAbilityPhaseInterrupted()
     if (not IsServer()) then
         return
     end
+    self:DestroyAbilityPhaseEffects()
+end
+
+function crystal_sorceress_frost_comet:DestroyAbilityPhaseEffects()
+    if (not IsServer()) then
+        return
+    end
+    StopSoundOn("Hero_Ancient_Apparition.ColdFeetCast", self.caster)
     ParticleManager:DestroyParticle(self.pidx, true)
     ParticleManager:ReleaseParticleIndex(self.pidx)
 end
 
-function crystal_sorceress_frost_comet:OnProjectileHit()
-    if (not IsServer()) then
+function crystal_sorceress_frost_comet:OnProjectileHit(target)
+    if (not IsServer() or not target) then
         return
     end
-    ParticleManager:DestroyParticle(self.pidx, false)
-    ParticleManager:ReleaseParticleIndex(self.pidx)
-    EmitSoundOn("Hero_Ancient_Apparition.ChillingTouch.Target", self.target)
-    local procChance = self:GetSpecialValueFor("proc_chance")
-    if (RollPercentage(procChance)) then
-        local pidx = ParticleManager:CreateParticle("particles/units/crystal_sorceress/frost_comet/crystal_sorceress_frost_comet_hit_b.vpcf", PATTACH_ABSORIGIN, self.target)
-        Timers:CreateTimer(2.0, function()
-            ParticleManager:DestroyParticle(pidx, false)
-            ParticleManager:ReleaseParticleIndex(pidx)
-        end)
-        local enemies = FindUnitsInRadius(self.caster:GetTeam(),
-                self.target:GetAbsOrigin(),
+    self:DestroyAbilityPhaseEffects()
+    if (RollPercentage(self.procChance)) then
+        local pidx = ParticleManager:CreateParticle("particles/units/crystal_sorceress/frost_comet/crystal_sorceress_frost_comet_hit_b.vpcf", PATTACH_ABSORIGIN, target)
+        ParticleManager:ReleaseParticleIndex(pidx)
+        local enemies = FindUnitsInRadius(self.casterTeam,
+                target:GetAbsOrigin(),
                 nil,
-                self:GetSpecialValueFor("proc_aoe"),
+                self.procAoe,
                 DOTA_UNIT_TARGET_TEAM_ENEMY,
                 DOTA_UNIT_TARGET_ALL,
                 DOTA_UNIT_TARGET_FLAG_NONE,
                 FIND_ANY_ORDER,
                 false)
-        local damage = Units:GetHeroIntellect(self.caster) * self:GetSpecialValueFor("proc_damage") * 0.01
+        local damage = Units:GetHeroIntellect(self.caster) * self.procDamage
         for _, enemy in pairs(enemies) do
             local damageTable = {}
             damageTable.caster = self.caster
@@ -78,18 +107,14 @@ function crystal_sorceress_frost_comet:OnProjectileHit()
             GameMode:DamageUnit(damageTable)
             local pidx = ParticleManager:CreateParticle("particles/units/crystal_sorceress/frost_comet/crystal_sorceress_frost_comet_hit_smoke.vpcf", PATTACH_POINT, enemy)
             ParticleManager:SetParticleControlEnt(pidx, 1, enemy, PATTACH_POINT_FOLLOW, "attach_hitloc", enemy:GetAbsOrigin(), true)
-            Timers:CreateTimer(1.0, function()
-                ParticleManager:DestroyParticle(pidx, false)
-                ParticleManager:ReleaseParticleIndex(pidx)
-            end)
+            ParticleManager:ReleaseParticleIndex(pidx)
         end
-        return
     end
     local damageTable = {}
     damageTable.caster = self.caster
-    damageTable.target = self.target
+    damageTable.target = target
     damageTable.ability = self
-    damageTable.damage = Units:GetHeroIntellect(self.caster) * self:GetSpecialValueFor("damage") * 0.01
+    damageTable.damage = Units:GetHeroIntellect(self.caster) * self.damage
     damageTable.frostdmg = true
     damageTable.single = true
     GameMode:DamageUnit(damageTable)
@@ -110,7 +135,41 @@ function crystal_sorceress_frost_comet:OnSpellStart()
     }
     ProjectileManager:CreateTrackingProjectile(projectile)
     EmitSoundOn("Hero_Ancient_Apparition.ChillingTouch.Cast", self.caster)
+    if (self.freezingDestructionCdrFlat > 0) then
+        local cooldownTable = {
+            target = self.caster,
+            ability = "crystal_sorceress_freezing_destruction",
+            reduction = self.freezingDestructionCdrFlat,
+            isflat = true
+        }
+        GameMode:ReduceAbilityCooldown(cooldownTable)
+    end
 end
+
+function crystal_sorceress_frost_comet:OnUpgrade()
+    self.damage = self:GetSpecialValueFor("damage") / 100
+    self.procDamage = self:GetSpecialValueFor("proc_damage") / 100
+    self.procChance = self:GetSpecialValueFor("proc_chance")
+    self.procAoe = self:GetSpecialValueFor("proc_aoe")
+    self.frostResistanceReduction = self:GetSpecialValueFor("frost_resistance_reduction") / -100
+    self.frostResistanceReductionDuration = self:GetSpecialValueFor("frost_resistance_reduction_duration")
+    self.freezingDestructionCdrFlat = self:GetSpecialValueFor("freezing_destruction_cdr_flat")
+    self.casterTeam = self:GetCaster():GetTeamNumber()
+end
+
+function crystal_sorceress_frost_comet:OnPostTakeDamage(damageTable)
+    local ability = damageTable.attacker:FindAbilityByName("crystal_sorceress_frost_comet")
+    if (ability and damageTable.ability and damageTable.ability == ability and ability.frostResistanceReduction and ability.frostResistanceReductionDuration > 0) then
+        local modifierTable = {}
+        modifierTable.ability = ability
+        modifierTable.target = damageTable.victim
+        modifierTable.caster = damageTable.attacker
+        modifierTable.modifier_name = "modifier_crystal_sorceress_frost_comet_debuff"
+        modifierTable.duration = ability.frostResistanceReductionDuration
+        GameMode:ApplyDebuff(modifierTable)
+    end
+end
+
 -- crystal_sorceress_deep_freeze modifiers
 modifier_crystal_sorceress_deep_freeze_aura = class({
     IsHidden = function(self)
@@ -757,5 +816,6 @@ end
 if (IsServer() and not GameMode.CRYSTAL_SORCERESS_INIT) then
     GameMode:RegisterPreDamageEventHandler(Dynamic_Wrap(modifier_crystal_sorceress_deep_freeze_aura_debuff_stacks, 'OnTakeDamage'))
     GameMode:RegisterPostDamageEventHandler(Dynamic_Wrap(modifier_crystal_sorceress_freezing_destruction, 'OnPostTakeDamage'))
+    GameMode:RegisterPostDamageEventHandler(Dynamic_Wrap(crystal_sorceress_frost_comet, 'OnPostTakeDamage'))
     GameMode.CRYSTAL_SORCERESS_INIT = true
 end
