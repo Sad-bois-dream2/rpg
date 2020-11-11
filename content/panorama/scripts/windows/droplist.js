c48757f8d6dac83f8feab359547748903c92d95c
fdsatrew <fdsatrewq3@gmail.com>
2020-11-11 15:48:04 +0300

    Adjusted drop list entry layout

--- content/panorama/scripts/windows/droplist.js
+++ content/panorama/scripts/windows/droplist.js
@@ -8,16 +8,20 @@ function GetHEXPlayerColor(playerId) {
 
 function OnItemDrop(event) {
 	var itemDropPanel = $.CreatePanel("Panel", dropContainer, "");
+	var itemDesiredSlot = ItemsDatabase.GetItemSlot(event.item);
+	var itemRarity = ItemsDatabase.GetItemRarity(event.item);
+    var itemRarityColor = ItemsDatabase.GetItemRarityColor(itemRarity);
 	itemDropPanel.BLoadLayout("file://{resources}/layout/custom_game/windows/droplist/droplist_item.xml", false, false);
 	var itemIcon = itemDropPanel.FindChildTraverse('DropItemIcon');
     itemIcon.itemname = event.item;
     itemDropPanel.FindChildTraverse('ItemOwnerIcon').heroname = event.hero;
     itemDropPanel.FindChildTraverse('ItemOwnerLabel').text = $.Localize("DOTA_DropList_FoundByPlayer").replace("%PLAYER_NAME%", "<font color='" + GetHEXPlayerColor(event.player_id) + "'>" + Players.GetPlayerName(event.player_id) + "</font>");
-    itemDropPanel.FindChildTraverse('ItemNameLabel').text = $.Localize("#DOTA_Tooltip_Ability_"+event.item);
-	var itemDesiredSlot = ItemsDatabase.GetItemSlot(event.item);
-	var itemRarity = ItemsDatabase.GetItemRarity(event.item);
+    var itemNameLabel = itemDropPanel.FindChildTraverse('ItemNameLabel');
+    itemNameLabel.text = $.Localize("#DOTA_Tooltip_Ability_"+event.item);
+    itemNameLabel.style.color = itemRarityColor;
     itemDropPanel.FindChildTraverse('ItemRarityLabel').text = ItemsDatabase.GetItemRarityName(itemRarity);
     itemDropPanel.FindChildTraverse('ItemTypeLabel').text = ItemsDatabase.GetItemSlotName(itemDesiredSlot);
+    itemDropPanel.FindChildTraverse('ItemTooltipImageBorder').style.border = "2px solid " + itemRarityColor;
     itemDropPanel.Data().itemName = event.item;
     itemDropPanel.Data().itemStats = JSON.parse(event.stats);
     dropContainer.MoveChildBefore(itemDropPanel, dropContainer.GetChild(0));
