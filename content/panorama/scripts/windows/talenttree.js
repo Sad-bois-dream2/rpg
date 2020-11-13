7f44cc9db250be7170a60979f12ef7a013606f2f
fdsatrew <fdsatrewq3@gmail.com>
2020-11-13 15:50:57 +0300

    Talent tree client side rework v0.0001

--- content/panorama/scripts/windows/talenttree.js
+++ content/panorama/scripts/windows/talenttree.js
@@ -6,8 +6,97 @@ var totalPointsLabel;
 var TALENT_PANEL = 0, TALENT_IMAGE = 1, TALENT_LEVEL = 2, TALENT_ID = 3;
 var TOOLTIP_PANEL = 0, TOOLTIP_IMAGE = 1, TOOLTIP_NAME = 2, TOOLTIP_DESCRIPTION = 3;
 
-function OnTalentClick(id) {
-	GameEvents.SendCustomGameEventToServer("rpg_talenttree_lvlup_talent", {"talent_id" : id});
+var TALENTS_CONTAINER, TALENTS_WINDOW;
+var TALENTS_LAYOUT = {
+    "lastColumn": -1
+};
+var talentsData = {
+    "talentsCount": 0,
+};
+
+function OnTalentTreeData(event) {
+    var parsedTalents = JSON.parse(event.talents);
+    for(var i=0; i < parsedTalents.length; i++) {
+        talentsData[parsedTalents[i].id] = parsedTalents[i].data
+    }
+    BuildTalentTree(parsedTalents);
+    talentsData.talentsCount += parsedTalents.length;
+    if(talentsData.talentsCount >= event.count) {
+	    GameEvents.SendCustomGameEventToServer( "talent_tree_get_state", {});
+	    TALENTS_LAYOUT[TALENTS_LAYOUT["lastColumn"]].SetHasClass("last", true);
+    }
+}
+
+function OnTalentTreeState(event) {
+    var talentPoints = event.points;
+    var parsedStateData = JSON.parse(event.talents);
+    for(var i=0; i < parsedStateData.length; i++) {
+        var talentId = parsedStateData[i].id;
+        var talentColumn = talentsData[talentId].Column;
+        var talentRow = talentsData[talentId].Row;
+        talentsData[talentId].panel.SetHasClass("disabled", parsedStateData[i].disabled);
+        talentsData[talentId].panel.levelLabel.text = parsedStateData[i].level + " / " + parsedStateData[i].maxlevel;
+    }
+    TALENTS_LAYOUT["TalentPointsLabel"].text = $.Localize("talent_tree_current_talent_points").replace("%POINTS%", talentPoints);
+}
+
+function BuildTalentTree(parsedTalents) {
+    for (var key in parsedTalents) {
+        var talentColumn = parsedTalents[key].data["Column"];
+        var talentRow = parsedTalents[key].data["Row"];
+        CreateTalentPanel(talentRow, talentColumn, parsedTalents[key].id);
+    }
+}
+
+function CreateTalentPanel(row, column, talentId) {
+    if (TALENTS_LAYOUT[column]) {
+        if (TALENTS_LAYOUT[column][row]) {
+            var talentPanel = $.CreatePanel("Panel", TALENTS_LAYOUT[column][row], "HeroTalent" + talentId);
+            talentPanel.BLoadLayout("file://{resources}/layout/custom_game/talent_tree/talent.xml", false, false);
+            talentPanel.hittest = true;
+            talentPanel.hittestchildren = false;
+			talentPanel.Data().ShowTalentTooltip = ShowTalentTooltip;
+			talentPanel.Data().HideTalentTooltip = HideTalentTooltip;
+			talentPanel.Data().OnTalentClick = OnTalentClick;
+			var talentImagePanel = talentPanel.FindChildTraverse("TalentImage");
+			if(talentImagePanel) {
+			    talentImagePanel.SetImage(talentsData[talentId].Icon);
+			}
+			if(column > TALENTS_LAYOUT["lastColumn"]) {
+			    TALENTS_LAYOUT["lastColumn"] = column;
+			}
+			talentsData[talentId].panel = talentPanel;
+			talentsData[talentId].panel.levelLabel = talentPanel.FindChildTraverse("TalentLevel");
+        } else {
+            TALENTS_LAYOUT[column][row] = CreateTalentRow(row, column);
+            TALENTS_LAYOUT[column].MoveChildAfter(TALENTS_LAYOUT[column][row], TALENTS_LAYOUT[column].GetChild(0));
+            CreateTalentPanel(row, column, talentId);
+        }
+    } else {
+        var talentColumnPanel = $.CreatePanel("Panel", TALENTS_CONTAINER, "");
+        talentColumnPanel.SetHasClass("TalentTreeColumn", true);
+        TALENTS_LAYOUT[column] = talentColumnPanel;
+        talentColumnPanel.hittest = false;
+        talentColumnPanel.hittestchildren = true;
+        var titleRow = CreateTalentRow(row, column, true);
+        var titleLabel = $.CreatePanel("Label", titleRow, "");
+        titleLabel.SetHasClass("TitleLabel", true);
+        titleLabel.text = $.Localize("#talent_tree_column_" + column + "_title");
+        titleRow.hittest = false;
+        titleRow.hittestchildren = true;
+        CreateTalentPanel(row, column, talentId);
+    }
+}
+
+function CreateTalentRow(row, column, isTitleRow) {
+    if (TALENTS_LAYOUT[column]) {
+        var talentRowPanel = $.CreatePanel("Panel", TALENTS_LAYOUT[column], "");
+        talentRowPanel.SetHasClass("TalentTreeRow", true);
+        if(isTitleRow == null) {
+            TALENTS_LAYOUT[column][row] = talentRowPanel;
+        }
+        return talentRowPanel
+    }
 }
 
 function ShowTalentTooltip(id) {
@@ -18,7 +107,7 @@ function ShowTalentTooltip(id) {
         var talentImage = talents[id-1][TALENT_PANEL].Data().talentImage;
         talentName = $.Localize("#DOTA_TalentTree_Talent_"+id);
         talentDescription = $.Localize("#DOTA_TalentTree_Talent_"+id+"_Description");
-        CreateTalentTooltip(talentImage, talentName, talentDescription, cursorPosition[0], cursorPosition[1]);
+        UpdateTalentTooltip(talentImage, talentName, talentDescription, cursorPosition[0], cursorPosition[1]);
     }
 
 }
@@ -27,17 +116,7 @@ function HideTalentTooltip(id) {
     tooltip[TOOLTIP_PANEL].style.visibility = "collapse";
 }
 
-function OnTalentTreeWindowCloseRequest() {
-	var window = $("#MainWindow");
-	window.style.visibility = "collapse";
-}
-
-function OnTalentTreeButtonClicked() {
-	var window = $("#MainWindow");
-	window.style.visibility = (window.style.visibility === "collapse") ? "visible" : "collapse";
-}
-
-function CreateTalentTooltip(icon, name, description, x, y) {
+function UpdateTalentTooltip(icon, name, description, x, y) {
 	tooltip[TOOLTIP_IMAGE].SetImage(icon);
 	tooltip[TOOLTIP_NAME].text = name.toUpperCase();
 	tooltip[TOOLTIP_DESCRIPTION].text = description;
@@ -52,79 +131,29 @@ function CreateTalentTooltip(icon, name, description, x, y) {
 	tooltip[TOOLTIP_PANEL].style.visibility = "visible";
 }
 
-
-function BuildTalentTree() {
-	var root = $("#TalentTree");
-	var totalLines = 6;
-	var totalColumns = 3;
-	var talentsPerColumn = 3;
-	var talentId = 1;
-	for(var i = 0; i < totalLines; i++) {
-		var talentsRow = $.CreatePanel("Panel", root, "");
-		talentsRow.SetHasClass("TalentsRow", true);
-		for(var j = 0; j < totalColumns; j++) {
-			var talentsColumn = $.CreatePanel("Panel", talentsRow, "");
-			talentsColumn.SetHasClass("TalentsColumn", true);
-			for(k = 0; k < talentsPerColumn; k++) {
-				var talent = $.CreatePanel("Panel", talentsColumn, "HeroTalent"+talentId);
-				talent.SetHasClass("disabled", true);
-				talent.BLoadLayout("file://{resources}/layout/custom_game/windows/talenttree/talenttree_talent.xml", false, false);
-				talent.Data().ShowTalentTooltip = ShowTalentTooltip;
-				talent.Data().HideTalentTooltip = HideTalentTooltip;
-				talent.Data().OnTalentClick = OnTalentClick;
-				talent.Data().talentImage = "file://{images}/custom_game/hud/talenttree/talent_"+talentId+".png";
-				var talentImageAndLevel = talent.GetChild(0);
-				var talentImage = talentImageAndLevel.FindChildTraverse('TalentImage');
-				var talentLevel = talentImageAndLevel.FindChildTraverse('TalentLevel');
-				talentImage.SetImage(talent.Data().talentImage);
-				talents.push([talent, talentImage, talentLevel, talentId]);
-				talentId++;
-			}
-		}
-	}
+function OnTalentClick(talentId, disabled) {
+    if(disabled == false) {
+        GameEvents.SendCustomGameEventToServer( "rpg_talent_tree_level_up_talent", {"id": talentId});
+        Game.EmitSound("General.SelectAction");
+    }
 }
 
-function OnTalentTreeTotalPointsUpdateRequest(event) {
-	totalPointsLabel.text = $.Localize("#DOTA_TalentTree_Total_Talent_Points").replace("%value%", event.amount);
+function OnTalentTreeWindowOpenRequest(event) {
+
 }
 
-function OnTalentTreeStateUpdateRequest(event) {
-	var parsedData = JSON.parse(event.data);
-	for(var i = 0; i < parsedData.length; i++) {
-		var index = parsedData[i].talent_id - 1;
-		talents[index][TALENT_PANEL].SetHasClass("disabled", parsedData[i].disabled);
-		talents[index][TALENT_PANEL].SetHasClass("lvlup", parsedData[i].lvlup);
-		talents[index][TALENT_LEVEL].text = parsedData[i].level + " / " + parsedData[i].maxlevel;
-	}
-}
+function OnTalentTreeWindowCloseRequest(event) {
 
-var TalentsDataRequired = false;
-function UpdateValues() {
-	if(hero == -1) {
-		var localPlayer = Players.GetLocalPlayer();
-		hero = Players.GetPlayerHeroEntityIndex(localPlayer);
-	} else {
-		if(!TalentsDataRequired) {
-	        BuildTalentTree();
-			GameEvents.SendCustomGameEventToServer("rpg_talenttree_require_player_talents_state", {});
-			TalentsDataRequired = true;
-		}
-	}
-}
-
-function AutoUpdateValues() {
-    UpdateValues();
-    $.Schedule(0.1, function() {
-        AutoUpdateValues();
-    });
 }
 
 (function() {
 	tooltip = [$("#ItemTooltip"), $("#ItemTooltipImage"), $("#ItemTooltipNameLabel"), $("#ItemTooltipLabel")];
-	totalPointsLabel = $("#TotalTalentPointsLabel");
-	GameEvents.Subscribe("rpg_talenttree_open_window_from_server", OnTalentTreeButtonClicked);
-	GameEvents.Subscribe("rpg_talenttree_close_window_from_server", OnTalentTreeWindowCloseRequest);
-	GameEvents.Subscribe("rpg_talenttree_require_player_talents_state_from_server", OnTalentTreeStateUpdateRequest);
-	GameEvents.Subscribe("rpg_talenttree_update_total_talents_points", OnTalentTreeTotalPointsUpdateRequest);
-	AutoUpdateValues();
+    TALENTS_WINDOW = $("#TalentsWindowContainer")
+    TALENTS_CONTAINER = $("#TalentTreeColumnsContainer");
+    TALENTS_LAYOUT["TalentPointsLabel"] = $("#TotalTalentPointsLabel");
+    GameEvents.Subscribe("rpg_talent_tree_get_talents_from_server", OnTalentTreeData);
+    GameEvents.Subscribe("rpg_talent_tree_get_state_from_server", OnTalentTreeState);
+	GameEvents.Subscribe("rpg_talent_tree_open_window_from_server", OnTalentTreeWindowOpenRequest);
+	GameEvents.Subscribe("rpg_talent_tree_close_window_from_server", OnTalentTreeWindowCloseRequest);
+	GameEvents.SendCustomGameEventToServer( "rpg_talent_tree_get_talents", {});
 })();
